7ef7ba5f0785423481abc303fd7f99cd7138c987
jrodgut <jrodgut@gmail.com>
2015-02-24 20:42:45 +0100

    added counter

--- RollABall/Assets/Scripts/PlayerController.cs
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,16 @@ public class PlayerController : MonoBehaviour
 {
 	public float speed;
 
+	public GUIText countText;
+
+	private int count;
+
+	void Start()
+	{
+		count = 0;
+		refreshCountText ();
+	}
+
 	// Update before physics calculation
 	void FixedUpdate () 
 	{
@@ -15,10 +25,19 @@ public class PlayerController : MonoBehaviour
 		rigidbody.AddForce(force * speed * Time.deltaTime);
 	}
 
+	//On collision with other objects
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.gameObject.tag == "PickUp") {
 			other.gameObject.SetActive(false);
+			count++;
+			refreshCountText ();
 		}
 	}
+
+	private void refreshCountText()
+	{
+		countText.text = "Counter: " + count.ToString ();
+	}
+
 }
