8efcbb2e136c8ad4344838742b3e7cbf1e703be4
jrodgut <jrodgut@gmail.com>
2015-02-24 18:52:10 +0100

    addde

---
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	// Update before physics calculation
+	void FixedUpdate () {
+		float horizontalMove = Input.GetAxis ("Horizontal");
+		float verticalMove = Input.GetAxis ("Vertical");
+
+		Vector3 force = new Vector3 (horizontalMove, 0.0f, verticalMove);
+		rigidbody.AddForce(force * speed * Time.deltaTime);
+	}
+}
